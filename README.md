b59093323478a550d77889ed1ebdd1a3094ef2a5
Kumar Kasimala <kkasimala@salesforce.com>
2021-02-02 19:44:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Simple Node with Express + MongoDB Server
+
+An easy way to get started with a Express server with MongoDB with Node.js. [Read more about it.](https://www.robinwieruch.de/mongodb-express-setup-tutorial/)
+
+## Features
+
+- Express
+- REST API
+- MongoDB
+
+## Requirements
+
+- [node & npm](https://nodejs.org/en/)
+- [git](https://www.robinwieruch.de/git-essential-commands/)
+
+## Installation
+
+- `git clone git@github.com:kkasimala/accounting-nodejs.git`
+- `cd accounting-nodejs`
+- `npm install`
+- [start MongoDB](https://www.mongodb.com/try/download/community) https://medium.com/@LondonAppBrewery/how-to-download-install-mongodb-on-windows-4ee4b3493514
+- `npm start`
+- optional: include _.env_ in your _.gitignore_
+
+### GET Routes
+
+- visit http://localhost:3000
+  - /messages
+  - /messages/1
+  - /users
+  - /users/1
+
+### Beyond GET Routes
+
+#### Postman
+
+- Install [Postman](https://www.getpostman.com/apps) to interact with REST API
+- Create a message with:
+  - URL: http://localhost:3000/messages
+  - Method: POST
+  - Body: raw + JSON (application/json)
+  - Body Content: `{ "text": "Hi again, World" }`
+- Delete a message with:
+  - URL: http://localhost:3000/messages/1
+  - Method: DELETE
---
+++ src/index.js
@@ -0,0 +1,99 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+var path = require('path');
+const bodyParser = require('body-parser');
+const ejs = require('ejs')
+import models, { connectDb } from './models';
+import routes from './routes';
+
+const app = express();
+
+// * Application-Level Middleware * //
+
+// Third-Party Middleware
+
+app.use(cors());
+
+// Built-In Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'views')));
+app.engine('html', ejs.renderFile);
+// Custom Middleware
+
+app.use(async (req, res, next) => {
+  req.context = {
+    models,
+    me: await models.User.findByLogin('rwieruch'),
+  };
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.render('login');
+});
+
+// * Routes * //
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+// * Start * //
+
+const eraseDatabaseOnSync = true;
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({}),
+    ]);
+
+    createUsersWithMessages();
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
+});
+
+// * Database Seeding * //
+
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'rwieruch',
+  });
+
+  const user2 = new models.User({
+    username: 'ddavids',
+  });
+
+  const message1 = new models.Message({
+    text: 'Published the Road to learn React',
+    user: user1.id,
+  });
+
+  const message2 = new models.Message({
+    text: 'Happy to release ...',
+    user: user2.id,
+  });
+
+  const message3 = new models.Message({
+    text: 'Published a complete ...',
+    user: user2.id,
+  });
+
+  await message1.save();
+  await message2.save();
+  await message3.save();
+
+  await user1.save();
+  await user2.save();
+};
